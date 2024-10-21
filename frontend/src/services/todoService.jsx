2943822ee37b69c5f16e1dea5e6794274977a4f9
Yavuz <yavuzcekilis@gmail.com>
2024-10-22 01:27:43 +0300

    BASE_URL changed

--- frontend/src/services/todoService.jsx
+++ frontend/src/services/todoService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://todo-app-with-react-fastapi.onrender.com/";
 
 export const todoService = {
   getAllTodos: async () => {
