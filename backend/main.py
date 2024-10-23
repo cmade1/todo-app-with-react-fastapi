b62b3a5f35178c08c6089cfa66ea531945146ac0
Yavuz <yavuzcekilis@gmail.com>
2024-10-23 18:35:28 +0300

    allow origins changed

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["https://cmade1.github.io"],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
